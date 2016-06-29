b1533996bc25888ba6f6dd31c3f65b0d22ab6304
Andrew Fleming <andrew@prevision.com.au>
2016-06-29 10:02:08 +0800

    Removed project specific template parts

--- template-parts/tile.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-printf( '<article %s>', genesis_attr( 'entry' ) );
-	genesis_entry_header_markup_open();
-		prevision_do_single_image();
-		prevision_related_entry_title_links();
-	genesis_entry_header_markup_close();
-echo '</article>';
